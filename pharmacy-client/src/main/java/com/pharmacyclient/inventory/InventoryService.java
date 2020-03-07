dc21b71e0dfa06ff1baf088cc541565db40805ba
Eric Cupiał <eric.cupial@gmail.com>
2020-03-07 16:04:51 +0100

    Fix issue with Maven and Lombok

--- pharmacy-client/src/main/java/com/pharmacyclient/inventory/InventoryService.java
+++ pharmacy-client/src/main/java/com/pharmacyclient/inventory/InventoryService.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.xml.ws.Endpoint;
 import java.util.Arrays;
 import java.util.List;
 
