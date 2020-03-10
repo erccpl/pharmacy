e8e75460749a75a74fe74ffca6e8d94a7e5d58fc
Eric Cupiał <eric.cupial@gmail.com>
2020-03-10 16:46:04 +0100

    Add class for generating dummy objects

--- pharmacy-backend/src/test/java/com/pharmacy/TestObjectFactory.java
+++ pharmacy-backend/src/test/java/com/pharmacy/TestObjectFactory.java
@@ -1,11 +1,15 @@
 package com.pharmacy;
 
+import com.pharmacy.inventory.InventoryItem;
 import com.pharmacy.product.Product;
 import com.pharmacy.user.User;
 import com.pharmacy.user.role.Role;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TestObjectFactory {
 
@@ -38,6 +42,40 @@ public class TestObjectFactory {
         dummyUser.setActive(1);
 
         return dummyUser;
+    }
+
+    private static InventoryItem newInventoryForProduct(Long productId) {
+        InventoryItem item = new InventoryItem();
+        item.getProduct().setId(productId);
+        item.setStock((int)(10.0 * Math.random()));
+        float salePrice = round((float)(10.0 * Math.random()), 2);
+        item.setSalePrice(salePrice);
+        //TODO: the Date type is pretty much deprecated
+        item.setExpirationDate(between(new Date(), new Date(2014, 02, 11)));
+        return item;
+    }
+
+
+
+
+
+
+    // ----------------------- Helper methods -----------------------------
+
+    public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+
+    public static Date between(Date startInclusive, Date endExclusive) {
+        long startMillis = startInclusive.getTime();
+        long endMillis = endExclusive.getTime();
+        long randomMillisSinceEpoch = ThreadLocalRandom
+                .current()
+                .nextLong(startMillis, endMillis);
+
+        return new Date(randomMillisSinceEpoch);
     }
 
 }
